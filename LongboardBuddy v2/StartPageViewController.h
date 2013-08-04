6a898f4c7ceec882fcbf9bc7f292799fe497235e
JonKalfayan <Jonk@Jonk.local>
2013-08-04 15:52:45 -0700

    Possibility of getting a "begin" page before stop watch starts, but does not work.

---
+++ LongboardBuddy v2/StartPageViewController.h
@@ -0,0 +1,14 @@
+//
+//  StartPageViewController.h
+//  LongboardBuddy v2
+//
+//  Created by JonKalfayan on 8/4/13.
+//  Copyright (c) 2013 Jonk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartPageViewController : UIViewController
+
+
+@end
